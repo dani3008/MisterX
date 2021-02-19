25a2ddfa7e3b30ae4e50ff81a592c10bc98d025f
dani3008 <wada1028@hs-karlsruhe.de>
2021-02-19 16:50:34 +0100

    init

---
+++ app/src/main/java/com/example/mrx/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mrx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
